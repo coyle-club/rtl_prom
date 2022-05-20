85a519f12914e2684e455227c693a91ac0dca08e
Tom Petr <trpetr@gmail.com>
2022-05-20 12:13:46 +0000

    more work

--- rtl_prom/__init__.py
+++ rtl_prom/__init__.py
@@ -5,18 +5,17 @@ from typing import List
 from prometheus_client import start_http_server, Gauge
 from dataclasses import dataclass
 from collections import defaultdict
+import time
+import logging
 
 
-@dataclass(eq=True, frozen=True)
+@dataclass
 class ConfigItem:
     match: dict
     metric_name: str
     value_fields: List[str]
     label_fields: List[str]
     description: str
-
-    def __hash__(self):
-        return hash(frozenset(self.match.items()))
 
     def matches(self, data):
         return all(str(data.get(k)) == str(v) for k, v in self.match.items())
@@ -29,22 +28,28 @@ class ConfigItem:
 @click.option("--port", type=int, default=8081)
 @click.argument("config_file", type=click.File("r"))
 def main(port, config_file):
+    logging.basicConfig(level=logging.INFO)
     config = [ConfigItem(**ci) for ci in json.load(config_file)]
     metrics = {}
     start_http_server(port)
     for line in sys.stdin:
         data = json.loads(line)
 
-        ci = None
-        for item in config:
-            if item.matches(data):
-                ci = item
-                break
-        if not ci:
+        try:
+            ci = next(item for item in config if item.matches(data))
+        except StopIteration:
             continue
+
+        if "time" in data:
+            data["timestamp"] = int(
+                time.mktime(time.strptime(data["time"], "%Y-%m-%d %H:%M:%S")) * 1000
+            )
 
         labels = ci.labels(data)
         for value_field in ci.value_fields:
+            if value_field not in data:
+                logging.warn("Value field %r not found in %r", value_field, data)
+                continue
             if ci.metric_name not in metrics:
                 metrics[ci.metric_name] = Gauge(
                     f"rtl_433_{ci.metric_name}",
